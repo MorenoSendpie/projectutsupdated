8ed615d89c571828002087dfbdbe518a8c1f69b5
Moreno <alesandromoreno7@gmail.com>
2021-10-24 13:51:15 +0700

    uts

---
+++ app/src/main/java/com/example/project/Activity/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.project.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.project.Adapter.CategoryAdapter;
+import com.example.project.Adapter.PopularAdapter;
+import com.example.project.Domain.CategoryDomain;
+import com.example.project.Domain.FoodDomain;
+import com.example.project.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter, adapter2;
+    private RecyclerView recyclerViewCategoryList, recyclerViewPopularList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+        recyclerViewPopular();
+        bottomNavigation();
+    }
+
+    private void bottomNavigation() {
+        FloatingActionButton floatingActionButton = findViewById(R.id.card_btn);
+        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CartListActivity.class));
+            }
+        });
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private void recyclerViewPopular() {
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewPopularList = findViewById(R.id.recyclerView2);
+        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<FoodDomain> foodlist = new ArrayList<>();
+        foodlist.add(new FoodDomain("California Roll", "pizza1", "California Roll adalah sushi berisi Ketimun, Kepiting dan Alpukat", 10000));
+        foodlist.add(new FoodDomain("Fish Cake Roll", "burger", "Fish Cake Roll adalah sushi berbahan dasar ikan dengan sedikit sayur", 11000));
+        foodlist.add(new FoodDomain("Katsu Roll", "pizza2", "Katsu Roll adalah sushi berbahan dasar ayam dengan sedikit sayur", 12000));
+        foodlist.add(new FoodDomain("Tempura Roll", "pizza2", "Tempura Roll adalah sushi berbahan dasar udang dengan sedikit sayur", 10500));
+
+        adapter2 = new PopularAdapter(foodlist);
+        recyclerViewPopularList.setAdapter(adapter2);
+
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> categoryList = new ArrayList<>();
+        categoryList.add(new CategoryDomain("Pizza", "cat_1"));
+        categoryList.add(new CategoryDomain("Burger", "cat_2"));
+        categoryList.add(new CategoryDomain("Hotdog", "cat_3"));
+        categoryList.add(new CategoryDomain("Drink", "cat_4"));
+        categoryList.add(new CategoryDomain("Dount", "cat_5"));
+
+        adapter = new CategoryAdapter(categoryList);
+        recyclerViewCategoryList.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/project/Activity/VerifyLocationActivity.java
@@ -0,0 +1,215 @@
+package com.example.project.Activity;
+
+import static com.example.project.Activity.MyApplication.CHANNEL_1_ID;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.project.R;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.LocationComponentOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import java.util.List;
+
+
+public class VerifyLocationActivity extends AppCompatActivity implements
+        OnMapReadyCallback, PermissionsListener {
+    private PermissionsManager permissionsManager;
+    private MapboxMap mapboxMap;
+    private MapView mapView;
+
+    private Button verifyBtn;
+    private NotificationManagerCompat notificationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+        setContentView(R.layout.activity_verify_location);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        verifyBtn=findViewById(R.id.verifyBtn);
+        verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(VerifyLocationActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+        VerifyLocationActivity.this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                enableLocationComponent(style);
+            }
+        });
+
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+
+            LocationComponentOptions customLocationComponentOptions = LocationComponentOptions.builder(this)
+                    .pulseEnabled(true)
+                    .build();
+
+
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                            .locationComponentOptions(customLocationComponentOptions)
+                            .build());
+
+
+            locationComponent.setLocationComponentEnabled(true);
+
+
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+
+            locationComponent.setRenderMode(RenderMode.NORMAL);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    @SuppressWarnings( {"MissingPermission"})
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+//.Bawah buat push notif
+
+
+    public void sendOnChannel1(View v){
+        String title ="Sushimi";
+        String message = "Checkout Berhasil";
+
+        Intent activityIntent = new Intent(this, VerifyLocationActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity( this,
+                0,activityIntent,0);
+
+        Intent broadcastIntent = new Intent(this,NotificationReceiver.class);
+        broadcastIntent.putExtra("toastMessage",message);
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0,broadcastIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_baseline_looks_one_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.BLUE)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
+                .build();
+
+
+        notificationManager.notify(1,notification);
+
+
+    }
+}
